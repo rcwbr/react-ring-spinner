ae0567eed2770690efb9610376bbf2c94a0a15a1
Eric Weber <eric@weber.lol>
2019-01-18 09:21:38 -0800

    restore babel loader test and include; fix exclude 'build' statement

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,12 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.(js|jsx)$/,
-				exclude: /(node_modules|build)/,
+				test: /\.jsx?$/,
+				include: path.resolve(__dirname, 'src'),
+				exclude: [
+					path.resolve(__dirname, 'build'),
+					path.resolve(__dirname, 'node_modules')
+				],
 				use: {
 					loader: 'babel-loader',
 					options: {
